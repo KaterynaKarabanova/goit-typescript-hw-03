874150c0f57ee7d099d0f0c0c173c0f771d64085
Kateryna <ikatya7575@gmaiil.com>
2023-10-05 14:22:17 +0300

    done

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,63 @@
-const key = new Key();
+// Key (Ключ)
 
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature() {
+    return this.signature;
+  }
+}
+
+// Person (Людина)
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey() {
+    return this.key;
+  }
+}
+
+// House (Дім)
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  private tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key) {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
-
-export {};
